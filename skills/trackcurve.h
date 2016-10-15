92d625a67b6d5d7968506a735664cc0473699bdc
The Mahi <mohammadmahdi76@gmail.com>
2016-10-15 11:07:57 +0200

    new skills

--- skills/trackcurve.h
+++ skills/trackcurve.h
@@ -13,15 +13,27 @@ public:
     DEF_SKILL(CSkillTrackCurve);
     virtual void generateFromConfig(CAgent *a);
     virtual CSkillConfigWidget* generateConfigWidget(QWidget *parent);
-    SkillProperty(CSkillTrackCurve, Vector2D, Origin, pos0);
-    SkillProperty(CSkillTrackCurve, CCurve*, Curve, curve);
-    SkillProperty(CSkillTrackCurve, double, RMax, r_max);
-    SkillProperty(CSkillTrackCurve, double, DeltaAngle, delta_angle);
-    SkillProperty(CSkillTrackCurve, Vector2D, LookAt, lookat);
-    SkillProperty(CSkillTrackCurve, Vector2D, ConstantDir, constDir);
-    SkillProperty(CSkillTrackCurve, double, Error, error);
-    SkillProperty(CSkillTrackCurve, bool, TrackStarted, trackstarted);
-    SkillProperty(CSkillTrackCurve, double, InitialVelocity, initialvel);
+    SkillProperty(CSkillTrackCurve, Vector2D, Origin, orig);
+    SkillProperty(CSkillTrackCurve, bool, ClockWise, CW);
+    SkillProperty(CSkillTrackCurve, bool, Slow,slow);
+    SkillProperty(CSkillTrackCurve, double , DesiredRad,desiredRad);
+    SkillProperty(CSkillTrackCurve, AngleDeg, StepAngel, stepAngel);
+    SkillProperty(CSkillTrackCurve, AngleDeg , EndAng, endAng);
+    SkillProperty(CSkillTrackCurve, Vector2D, RobotAng,robotAng);
+private:
+    QList <Vector2D> path;
+    Vector2D nextPoint;
+    CNewBangBang *bangBang;
+    double rad;
+    Circle2D curve;
+    Vector2D agentPos;
+    Vector2D agentDir;
+    Vector2D agentVel;
+
+    Vector2D firstPos;
+    Vector2D finalPos;
+    _PID *curvePID;
+
 };
 
 #endif // TRACKCURVE_H
