f3619b57d4e93a8b17fe400819ff84bd08e9b02c
mohammad mahdi Rahimi <mohammadmahdi76@gmail.com>
2017-01-27 10:15:04 +0000

    Update widgets.cpp

--- ui/widgets.cpp
+++ ui/widgets.cpp
@@ -43,7 +43,6 @@ CTabDockWidget::CTabDockWidget(QWidget* parent , bool autoHide)
     tabs = new QTabWidget(w);
     QGridLayout *layout = new QGridLayout(w);
     tabs->setTabPosition(QTabWidget::West);
-    this->setFixedSize(400,400);
     if( autoHide ){
         isHide = hideable = false;
         btnAutoHide = new QPushButton(this);
