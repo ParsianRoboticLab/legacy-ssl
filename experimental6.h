466a768b0611b5c0be3af5c3fa1324673e14ccda
mohammad shirazi <mhmmdshirazi@gmail.com>
2017-01-29 19:03:05 +0330

    playmake doesn't go to the opponent penalty area anymore

--- experimental6.h
+++ experimental6.h
@@ -10,7 +10,7 @@
 clock_t t;
 //#define speedTest
 
-//#define kickTest
+#define kickTest
 QList <Vector2D> agentpath;
 void CMainApplication::Experimental6()
 {
@@ -26,7 +26,7 @@ void CMainApplication::Experimental6()
         stopFlag = false;
     if(knowledge->joystick->getButton4())
         stopFlag = true;
-    int skillAgent = 0;
+    int skillAgent = 3;
 
 
 
@@ -43,24 +43,6 @@ void CMainApplication::Experimental6()
     return;
 #endif
 
-
-    static CSkillDribble mD(soccer->agents[skillAgent]);
-    mD.setInitialLook(Vector2D(10000000,0));
-    mD.setTarget(knowledge->getAgent(4)->pos());
-    mD.setKickTol(2);
-    mD.setKickSpeed(500);
-    mD.setDoPass(true);
-    mD.execute();
-
-    /////////////////
-    static CSkillKick myKick(knowledge->getAgent(7));
-    myKick.setTarget(wm->field->ourGoal());
-    myKick.setChip(false);
-    myKick.setKickSpeed(1);
-    myKick.execute();
-
-
-    return;
     static bool catchTheBall = false;
     static CSkillGotoPointAvoid mGP(soccer->agents[skillAgent]);
     static int timer = 0;
@@ -86,18 +68,10 @@ void CMainApplication::Experimental6()
     if(dribblerArea.contains(wm->ball->pos))
     {
         if(!catchTheBall)
-        {
-            soccer->agents[skillAgent]->setRoller(1);
-
             mGP.execute();
-        }
         else
-        {
-            soccer->agents[skillAgent]->setRoller(6);
-
-            soccer->agents[skillAgent]->setRobotVel(0,0,2);
-        }
-
+            soccer->agents[skillAgent]->setRobotVel(-1,0,0);
+        soccer->agents[skillAgent]->setRoller(5);
         timer ++;
     }
     else
@@ -116,10 +90,10 @@ void CMainApplication::Experimental6()
     {
         catchTheBall = false;
     }
+    draw(dribblerArea,QColor(Qt::red));
 
 
-
-
+    draw(mousePos,1,QColor(Qt::black));
     return;
 
     static CSkillGotoPointAvoid mgpa(soccer->agents[1]);
