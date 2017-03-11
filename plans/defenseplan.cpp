7608b0a856bc4ec387a5c3205552e2dd6691773f
= <=>
2017-03-11 16:27:33 +0330

    some changes knowlegde for opp agents to be mark by new name ToBeMark

--- plans/defenseplan.cpp
+++ plans/defenseplan.cpp
@@ -3667,6 +3667,8 @@ void DefensePlan::findOppAgentsToMark(QList <Vector2D> _realDefTargets)
 
     }
 
+    knowledge->ToBeMark.clear();
+    knowledge->ToBeMark.append(oppAgentsToMarkPos);
 
     // qSort(oppAgentsToMarkPos.begin(),oppAgentsToMarkPos.end(),CMarkPlan::sortBy);
 
