ff89625934994d17db8729aa876006d7b4a961a0
mohammad mahdi Rahimi <mohammadmahdi76@gmail.com>
2017-04-05 10:40:15 +0430

    refine start bash

--- start.bash
+++ start.bash
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-cmds="HS skKpPfFiItTgGbB"
-
 while true;
 do
-ref=`cat ./REF.dat`
-cmd=`echo ${cmds:$ref:1}`
-echo $cmd
-#./ai mode real ref $cmd
-#killall ai
+./ai mode real ref s
+killall ai
 done;
