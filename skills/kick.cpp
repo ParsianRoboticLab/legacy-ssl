a900a2ae283f7d914142dc8400c0e45a17680e60
parsa <parsa.abdollahi.pa@gmail.com>
2017-07-07 19:21:41 +0430

    minor bugs

--- skills/kick.cpp
+++ skills/kick.cpp
@@ -1357,9 +1357,8 @@ double CSkillKick::kickTimeEstimation(CAgent *_agent, Vector2D _target)
     Circle2D robotAreaNear (_agent->pos(),0.1);
     if(wm->ball->vel.length() > 0.2)
     {
-        for(double i = 0 ; i < 3 ; i += 0.01)
+        for(double i = 0 ; i < 3 ; i += 0.03)
         {
-
             ballPosInFuture = wm->ball->getPosInFuture(i);
             finalPos = ballPosInFuture - (_target-ballPosInFuture).norm()*0.11;
             if(CSkillGotoPointAvoid::timeNeeded(_agent,finalPos,conf()->BangBang_VelMax(),ourRelax,oppRelax,true,0.2,true)<= i+0.1)
