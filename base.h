26a0f0d8292d6267dcf9a104f70139750bde800a
Fateme <fateme.hashemi23@gmail.com>
2017-07-18 11:11:26 +0430

    Add mix team field parameteres

--- base.h
+++ base.h
@@ -218,6 +218,8 @@ struct GameStatePacket
 #define _PARSIAN_BOUNDRY         4.40
 #define _MAX_DIST               sqrt(_FIELD_WIDTH * _FIELD_WIDTH + _FIELD_HEIGHT * _FIELD_HEIGHT)
 
+#define _MIXTEAM_FIELD_WIDTH            13.4
+#define _MIXTEAM_FIELD_HEIGHT           9.00
 
 /* Common Includes */
 #include "geom.h"
